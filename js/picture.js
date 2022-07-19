24a859086f97cf79f0d4276c21eb98f0bf588415
Быков Максим <bykovme@krista.ru>
2022-07-19 18:48:40 +0300

    Отрисовал сгенерированные DOM-элементы в блок .pictures fix

--- js/picture.js
+++ js/picture.js
@@ -5,16 +5,12 @@ const template = templateFragment.querySelector('a');
 const fragment = document.createDocumentFragment();
 const pictures = document.querySelector('.pictures');
 
-console.log(pictures.children[0].children[0]);
-
 for (let i = 1; i < 26; i++) {
   const photo = createRandomPhoto(i);
   const element = template.cloneNode(true);
   element.querySelector('img').src = photo.url;
   element.querySelector('.picture__likes').textContent = photo.likes;
   element.querySelector('.picture__comments').textContent = photo.comments.length;
-  //element.classList.add(`el-${  i + 1}`); // Добавляем порядковый класс, который начинается с единицы, а не с нуля, поэтому 'i + 1'
-  //element.children[0].textContent = i; // Записываем содержимое
   fragment.appendChild(element);
 }
 
